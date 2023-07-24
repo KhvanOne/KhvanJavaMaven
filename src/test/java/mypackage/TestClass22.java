c6b4d590820391193b78b9d1bed44cdf83ad953e
KhvanOne <khvanone1@gmail.com>
2023-07-24 14:24:05 +0500

    the end

---
+++ src/test/java/mypackage/TestClass22.java
@@ -0,0 +1,24 @@
+package mypackage;
+
+public class TestClass22 {
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        cat.name = "Vas`ka";
+        dog.name = "Nikitos";
+
+        cat.color = "White";
+        dog.color = "Blue";
+
+        cat.walk("Backyard");
+        dog.walk("Park");
+
+        cat.feed("Milk");
+        dog.sit();
+
+        System.out.println("Cat name is: " + cat.name);
+        System.out.println("Dog name is: " + dog.name);
+
+    }
+}
